34ede777e7ed0a15612cf7226fb00a358ebc0534
Uche Ogbu <Ucheogbu95@gmail.com>
2019-07-26 15:52:40 +0100

    v5

--- Ruper/forms.py
+++ Ruper/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import UserData
+import re
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -8,6 +9,9 @@ class CustomUserCreationForm(UserCreationForm):
             self.errors['password1'] = 'Password Must Be at Least 16 characters'
         if self.cleaned_data['password1'] and self.cleaned_data['password2'] and self.cleaned_data['password1'] != self.cleaned_data['password2']:
             self.errors['password1'], self.errors['password1'] = 'Passwords don"t match', 'Passwords don"t match'
+        if len(re.findall(r'^[\W{2}\w+\d{3}.@+-]+$', self.cleaned_data['password1'])) == 0:
+            self.errors['password1'] = 'Password Must start with two(2) Capital Letters,Contain at least three(3) ' \
+                                       'Numbers and contain at least ' 'two(2) Symbols'
 
     class Meta(UserCreationForm):
         model = UserData
